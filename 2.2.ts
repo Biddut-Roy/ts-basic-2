3b1bf20f7bc15c984d146576e6da5a7b9be657a3
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-03 10:00:52 +0600

    type and interface function and array setup

--- 2.2.ts
+++ 2.2.ts
@@ -4,14 +4,14 @@
         age: number
     }
 
-    type userWithrole1 = User1 & {role: string}
+    type userWithrole1 = User1 & { role: string }
 
     interface User2 {
         name: string,
         age: number
     }
 
-    interface userWithrole2 extends User2 {role: string}
+    interface userWithrole2 extends User2 { role: string }
 
 
     const user: userWithrole2 = {
@@ -19,4 +19,21 @@
         age: 36,
         role: 'admin'
     }
+
+
+    type roleNumber1 = number[]
+    interface roleNumber2 {
+        [index: number]: number
+    }
+
+    const add: roleNumber2 = [1, 2, 3]
+    
+    type roleNumber3 = (add1 : number, add2 : number,) => number
+     interface roleNumber4 {
+        (add1 : number, add2 : number): number
+     }
+
+
+    const addRole: roleNumber4 = (add1 , add2) => add1 + add2
+
 }
