d321f746840cf16170ae52812b3d40db19b9b0ad
Biddut-Roy <biddut.chandro.roy2@gmail.com>
2024-05-03 17:12:39 +0600

    Constraints In Typescript

--- 2.6.ts
+++ 2.6.ts
@@ -1,3 +1,10 @@
 {
     // Constraints In Typescript
+    const addCourchToStudent =< T extends { id: number;  name: string; email: string;}> (student : T) => {
+        const course = " Next lavel Web development"
+
+        return {
+            ...student, course
+        }
+    }
 }
